c677a0331d805ed9e6f2e3190dcc64d613446800
RafaellyG <rafaelly.gruber@gmail.com>
2019-10-09 05:37:11 -0300

    melhorias e arquivo de dist

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDT34RdWx5f1JPv7JQkk71Z4J5z9BQjOmQ",
+    authDomain: "auction-system-37b6e.firebaseapp.com",
+    databaseURL: "https://auction-system-37b6e.firebaseio.com",
+    projectId: "auction-system-37b6e",
+    storageBucket: "auction-system-37b6e.appspot.com",
+    messagingSenderId: "825515862970",
+    appId: "1:825515862970:web:fadf23df41cc1cc8e90b83",
+    measurementId: "G-29FP8EPXDS"
+  }
 };
